aaeaaa27a968863e76a6c9f9d7ff0554f812f3d6
Robby Hernowo <robby@indotim.com>
2022-10-03 17:37:23 +0700

    import adminlte

---
+++ database/migrations/2022_10_03_170444_create_transactions_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('service_id')->constrained('services')->onDelete('cascade');
+            $table->foreignId('service_detail_id')->constrained('service_detail')->onDelete('cascade');
+            $table->string('name');
+            $table->string('description');
+            $table->enum('status', ['antrian', 'proses', 'selesai']);
+            $table->enum('payment_status', ['belum bayar', 'sudah bayar']);
+            $table->string('payment_proof');
+            $table->float('total_price');
+            $table->string('transaction_code');
+            $table->string('address');
+            $table->string('phone_number');
+            $table->string('service_date');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
